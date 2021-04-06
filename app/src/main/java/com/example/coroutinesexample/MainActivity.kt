9aa61e57ba4b8b3787ca2af06ef67c4b4e5f25c3
Kadir Kuruca <kadirkuruca572@gmail.com>
2021-04-06 22:39:03 +0300

    Very basic example had done for coroutines

--- app/src/main/java/com/example/coroutinesexample/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesexample/MainActivity.kt
@@ -2,10 +2,70 @@ package com.example.coroutinesexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private val RESULT_1 = "Result 1"
+    private val RESULT_2 = "Result 2"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStart.setOnClickListener{
+
+            // IO =>  For Network request, database actions
+            // Main => Doing this on main thread so interacting with UI
+            // Default => Heavy Computational things : Filter a large list
+            CoroutineScope(IO).launch {
+                fakeApiRequest()
+            }
+        }
+    }
+
+    private fun setNewText(input : String){
+        val newText = textView.text.toString()+ "\n$input"
+        textView.text = newText
+    }
+
+    private suspend fun setTextOnMainThread(input : String){
+        withContext(Main){
+            setNewText(input)
+        }
+    }
+
+    private suspend fun fakeApiRequest(){
+        val result1 = getResult1FromApi()
+        setTextOnMainThread(result1)
+
+        val result2 = getResult2FromApi(result1)
+        setTextOnMainThread(result2)
+    }
+
+    private suspend fun getResult1FromApi() : String{
+        logThread("getResult1FromApi")
+        delay(1000)
+        return RESULT_1
+    }
+
+    private suspend fun getResult2FromApi(result1 : String) : String{
+        logThread("getResult2FromApi")
+        delay(1000)
+        return if(result1 == "Result 1")
+            RESULT_2
+        else
+            ""
+    }
+
+    private fun logThread(methodName : String){
+        println("debug : $methodName : ${Thread.currentThread().name}")
     }
 }
